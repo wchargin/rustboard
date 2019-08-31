d2c70bb2d1b7264e1dd8338b88f8d3499e1994ca
William Chargin <wchargin@gmail.com>
2019-08-30 22:52:39 -0700

    Implement basic raw file reading in 8-byte chunks

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::env;
+use std::fs::File;
+use std::io;
+use std::io::Read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,4 +15,35 @@ fn main() {
         std::process::exit(1);
     };
     println!("Processing file: {}", events_file);
+    read_events(events_file).unwrap_or_else(|e| {
+        eprintln!("error: {:?}", e);
+        std::process::exit(1);
+    });
+}
+
+fn read_events(filename: &str) -> io::Result<()> {
+    let mut file = File::open(filename)?;
+    while let Some(event) = read_event(&mut file)? {
+        println!("block: {:?}", event);
+    }
+    Ok(())
+}
+
+/// Returns the read TF record, or `None` if at EOF.
+fn read_event(f: &mut File) -> io::Result<Option<Vec<u8>>> {
+    // From TensorFlow `record_writer.cc` comments:
+    // Format of a single record:
+    //  uint64    length
+    //  uint32    masked crc of length
+    //  byte      data[length]
+    //  uint32    masked crc of data
+    //
+    // For now, just read 8-byte blocks.
+    let mut buf = [0 as u8; 8];
+    match f.read_exact(&mut buf) {
+        Ok(()) => (),
+        Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => return Ok(None),
+        e => e?,
+    };
+    Ok(Some(buf.to_vec()))
 }
