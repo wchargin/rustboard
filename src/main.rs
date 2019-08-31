d2db4e42762e55d4cd08bb9b1e2c04b9630b2c76
William Chargin <wchargin@gmail.com>
2019-08-30 23:20:28 -0700

    Implement TFRecord reading
    We don’t bother computing CRCs, but we do read properly delimited
    records. Run on the test data to see `brain.Event:2` in the first
    record, followed by alternating records with tags `accuracy` and `loss`
    (and a bunch of binary data in the middle of each record).

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use byteorder::{LittleEndian, ReadBytesExt};
 use std::env;
 use std::fs::File;
 use std::io;
@@ -23,27 +24,34 @@ fn main() {
 
 fn read_events(filename: &str) -> io::Result<()> {
     let mut file = File::open(filename)?;
-    while let Some(event) = read_event(&mut file)? {
-        println!("block: {:?}", event);
+    loop {
+        match read_event(&mut file) {
+            Ok(block) => println!(
+                "read block of length {}: {:?}",
+                block.len(),
+                String::from_utf8_lossy(&block)
+            ),
+            Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => break,
+            Err(e) => return Err(e),
+        }
     }
     Ok(())
 }
 
-/// Returns the read TF record, or `None` if at EOF.
-fn read_event(f: &mut File) -> io::Result<Option<Vec<u8>>> {
+/// Returns the read TF record, if possible.
+fn read_event(f: &mut File) -> io::Result<Vec<u8>> {
     // From TensorFlow `record_writer.cc` comments:
     // Format of a single record:
     //  uint64    length
     //  uint32    masked crc of length
     //  byte      data[length]
     //  uint32    masked crc of data
-    //
-    // For now, just read 8-byte blocks.
-    let mut buf = [0 as u8; 8];
-    match f.read_exact(&mut buf) {
-        Ok(()) => (),
-        Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => return Ok(None),
-        e => e?,
-    };
-    Ok(Some(buf.to_vec()))
+    let len: u64 = f.read_u64::<LittleEndian>()?;
+    f.read_u32::<LittleEndian>()?; // skip length checksum
+    let mut buf: Vec<u8> = Vec::with_capacity(len as usize);
+    if f.take(len).read_to_end(&mut buf)? < len as usize {
+        return Err(io::Error::from(io::ErrorKind::UnexpectedEof));
+    }
+    f.read_u32::<LittleEndian>()?; // skip data checksum
+    Ok(buf)
 }
