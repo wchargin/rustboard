2e6bda6e6731da5ec84c1017baedbc3ddeecf055
William Chargin <wchargin@gmail.com>
2019-08-30 22:21:47 -0700

    Read an events file name as CLI argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let events_file = if args.len() == 2 {
+        &args[1]
+    } else {
+        eprintln!(
+            "usage: {} EVENTS_FILE",
+            args.first().map(String::as_ref).unwrap_or("rustboard")
+        );
+        std::process::exit(1);
+    };
+    println!("Processing file: {}", events_file);
 }
